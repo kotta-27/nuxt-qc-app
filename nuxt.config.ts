8c4c7875fad0841ae8b1c4511b60d700fe98ade1
kotta-27 <al20013@shibaura-it.ac.jp>
2025-06-15 01:16:04 +0900

    [update]titleの変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "量子回路シミュレータ",
+      meta: [
+        { name: "description", content: "量子回路シミュレータ" },
+      ],
+    },
+  },
 })
